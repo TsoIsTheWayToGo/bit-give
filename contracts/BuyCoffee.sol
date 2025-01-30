b2a96430ee970cef6a5bae1f2b34d843fbeb41a7
TsoIsTheWayToGo <ericgordontso@gmail.com>
2025-01-30 15:34:13 -0600

    Create smart contract

---
+++ contracts/BuyCoffee.sol
@@ -0,0 +1,10 @@
+//SPDX=License-identifier: MIT
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract BuyCoffee {
+  constructor() payable {
+    console.log("Initialize Smart Contract")
+  }
+}
