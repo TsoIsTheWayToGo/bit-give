29528a842f40ad2ff5af131573addc160b66cd84
TsoIsTheWayToGo <ericgordontso@gmail.com>
2025-01-30 15:35:21 -0600

    Add coffee array

--- contracts/BuyCoffee.sol
+++ contracts/BuyCoffee.sol
@@ -4,6 +4,17 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract BuyCoffee {
+
+  struct Coffee {
+    address supporter;
+    string message;
+    string name;
+    uint256 amount;
+    uint256 timestamp;
+  }
+
+  Coffee[] coffee;
+
   constructor() payable {
     console.log("Initialize Smart Contract")
   }
